67852e7f979b193735eb20499f2349353a723967
Alexander Zakatnov <alexander.zakatnov@rokolabs.com>
2018-03-16 13:41:20 +0400

    Initial Commit

---
+++ zira/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  zira
+//
+//  Created by Alexander Zakatnov on 16.03.2018.
+//  Copyright © 2018 Zaakk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
